51701a43307fa9ee665ff8a47660a12f77ebfe0e
Sam <sam@taylor.org.uk>
2020-01-05 16:10:41 +0000

    Add project files.

---
+++ Audio Manipulation 2/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿using System;
+
+namespace Audio_Manipulation_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butPlay = new System.Windows.Forms.Button();
+            this.butBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.waveformPainter2 = new NAudio.Gui.WaveformPainter();
+            this.butPause = new System.Windows.Forms.Button();
+            this.butStop = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.butPauseStop = new System.Windows.Forms.Button();
+            this.volumeSlider1 = new NAudio.Gui.VolumeSlider();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // butPlay
+            // 
+            this.butPlay.Location = new System.Drawing.Point(8, 12);
+            this.butPlay.Name = "butPlay";
+            this.butPlay.Size = new System.Drawing.Size(75, 45);
+            this.butPlay.TabIndex = 0;
+            this.butPlay.Text = "Play";
+            this.butPlay.UseVisualStyleBackColor = true;
+            this.butPlay.Click += new System.EventHandler(this.OnButtonPlayClick_Click);
+            // 
+            // butBrowse
+            // 
+            this.butBrowse.Location = new System.Drawing.Point(8, 221);
+            this.butBrowse.Name = "butBrowse";
+            this.butBrowse.Size = new System.Drawing.Size(75, 27);
+            this.butBrowse.TabIndex = 2;
+            this.butBrowse.Text = "Browse File";
+            this.butBrowse.UseVisualStyleBackColor = true;
+            this.butBrowse.Click += new System.EventHandler(this.ButBrowse_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(8, 255);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(794, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(8, 72);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1197, 100);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // waveformPainter2
+            // 
+            this.waveformPainter2.Location = new System.Drawing.Point(0, 0);
+            this.waveformPainter2.Name = "waveformPainter2";
+            this.waveformPainter2.Size = new System.Drawing.Size(0, 0);
+            this.waveformPainter2.TabIndex = 0;
+            // 
+            // butPause
+            // 
+            this.butPause.Location = new System.Drawing.Point(808, 12);
+            this.butPause.Name = "butPause";
+            this.butPause.Size = new System.Drawing.Size(69, 45);
+            this.butPause.TabIndex = 6;
+            this.butPause.Text = "Pause";
+            this.butPause.UseVisualStyleBackColor = true;
+            this.butPause.Click += new System.EventHandler(this.butPause_Click);
+            // 
+            // butStop
+            // 
+            this.butStop.Location = new System.Drawing.Point(883, 12);
+            this.butStop.Name = "butStop";
+            this.butStop.Size = new System.Drawing.Size(69, 45);
+            this.butStop.TabIndex = 7;
+            this.butStop.Text = "Stop";
+            this.butStop.UseVisualStyleBackColor = true;
+            this.butStop.Click += new System.EventHandler(this.butStop_click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(808, 255);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(144, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // butPauseStop
+            // 
+            this.butPauseStop.Location = new System.Drawing.Point(89, 12);
+            this.butPauseStop.Name = "butPauseStop";
+            this.butPauseStop.Size = new System.Drawing.Size(75, 45);
+            this.butPauseStop.TabIndex = 9;
+            this.butPauseStop.Text = "Pause/Stop";
+            this.butPauseStop.UseVisualStyleBackColor = true;
+            this.butPauseStop.Click += new System.EventHandler(this.butPauseStop_Click);
+            this.butPauseStop.MouseClick += new System.Windows.Forms.MouseEventHandler(this.butPauseStop_MouseClick);
+            // 
+            // volumeSlider1
+            // 
+            this.volumeSlider1.Location = new System.Drawing.Point(808, 229);
+            this.volumeSlider1.Name = "volumeSlider1";
+            this.volumeSlider1.Size = new System.Drawing.Size(144, 19);
+            this.volumeSlider1.TabIndex = 10;
+            this.volumeSlider1.Load += new System.EventHandler(this.volumeSlider1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(961, 278);
+            this.Controls.Add(this.volumeSlider1);
+            this.Controls.Add(this.butPauseStop);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.butStop);
+            this.Controls.Add(this.butPause);
+            this.Controls.Add(this.waveformPainter2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.butBrowse);
+            this.Controls.Add(this.butPlay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butPlay;
+        private System.Windows.Forms.Button butBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private NAudio.Gui.WaveformPainter waveformPainter2;
+        private System.Windows.Forms.Button butPause;
+        private System.Windows.Forms.Button butStop;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button butPauseStop;
+        private NAudio.Gui.VolumeSlider volumeSlider1;
+    }
+}
+
---
+++ Audio Manipulation 2/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using NAudio.FileFormats;
+using WaveFormRendererLib;
+using System.Drawing.Imaging;
+using System.Drawing;
+
+
+
+namespace Audio_Manipulation_2
+{
+    public partial class Form1 : Form
+    {
+
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+
+        double numClicktime = 0;
+        private Timer doubleClickTimer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void Form1_Load(object sender, EventArgs e)
+        // {
+        //   Image image = Image.FromFile("c:/output.png");
+        //   pictureBox2.Image = image;
+        //   pictureBox2.Height = image.Height;
+        //   pictureBox2.Width = image.Width;
+        // }
+
+
+
+        private void OnButtonPlayClick_Click(object sender, EventArgs e)
+        {
+
+            if (outputDevice == null)
+            {
+                outputDevice = new WaveOutEvent();                      //WaveOutEvent best option for sending audio to soundcard 
+                outputDevice.PlaybackStopped += OnPlaybackStopped;      // to add ASIO - github.com/naudio/NAudio/blob/master/Docs/AsioPlayback.md
+            }
+            if (audioFile == null)
+            {                                                               
+                audioFile = new AudioFileReader(textBox1.Text);                             //audioFile = new AudioFileReader(@"c:\aphextwin.mp3");     //hardcoded directory
+                outputDevice.Init(audioFile);
+            }
+            else
+            {
+            
+            }
+            outputDevice.Play();
+
+        }
+        public void OnPlaybackStopped(object sender, StoppedEventArgs args)  //stop playback event handler
+        {
+            outputDevice.Dispose();
+            outputDevice = null;
+            audioFile.Dispose();
+            audioFile = null;       //resets playback to 0/null 
+        }
+
+        private void ButBrowse_Click_1(object sender, EventArgs e)      //browse button
+        {
+            string oSelectedFile = "";
+            System.Windows.Forms.OpenFileDialog oDlg = new System.Windows.Forms.OpenFileDialog();
+            if (System.Windows.Forms.DialogResult.OK == oDlg.ShowDialog())
+            {
+                oSelectedFile = oDlg.FileName;      // selected file = oSelectedFile
+                textBox1.Text = oSelectedFile;
+
+                // Configure Providers
+                MaxPeakProvider maxPeakProvider = new MaxPeakProvider();
+                RmsPeakProvider rmsPeakProvider = new RmsPeakProvider(200); // e.g. 200
+                SamplingPeakProvider samplingPeakProvider = new SamplingPeakProvider(200); // e.g. 200
+                AveragePeakProvider averagePeakProvider = new AveragePeakProvider(4); // e.g. 4
+
+                // Configure the style of the audio wave image
+                StandardWaveFormRendererSettings myRendererSettings = new StandardWaveFormRendererSettings();
+                myRendererSettings.Width = 1080;
+                myRendererSettings.TopHeight = 64;
+                myRendererSettings.BottomHeight = 64;
+
+                WaveFormRenderer renderer = new WaveFormRenderer();
+                String audioFilePath = textBox1.Text;
+                Image image = renderer.Render(audioFilePath, averagePeakProvider, myRendererSettings);
+                string folder = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+                System.IO.Directory.CreateDirectory(folder + "\\AudioManipulation");
+                image.Save(folder + "\\AudioManipulation\\output.png", ImageFormat.Png);
+
+                AddToLog("- Sound Played");
+
+                //loads waveform after generation on file browse
+                Image loadImage = Image.FromFile("\\output.png");
+                pictureBox2.Image = image;
+                pictureBox2.Height = image.Height;
+                pictureBox2.Width = image.Width;
+
+
+            }
+        }
+
+
+        public static void AddToLog(string txtToAdd)
+        {
+
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            System.IO.Directory.CreateDirectory(folder + "\\AudioManipulation");
+
+            string path = folder + "\\AudioManipulation" + "\\logfile.txt";
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(DateTime.Now.ToString("dd MMM yy HH:mm:ss") + " : " + txtToAdd);
+            }
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)  //text field for selected file directory
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)   //picturebox to display waveform
+        {
+
+        }
+
+        public static void AddToProgramData()
+        {
+
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            System.IO.Directory.CreateDirectory(folder + "\\AudioManipulation");
+            string path = folder + "\\AudioManipulation" + "\\logfile.txt";
+            using (StreamWriter sw = File.AppendText(path)) ;
+
+        }
+
+        private void butPause_Click(object sender, EventArgs e)
+        { 
+            outputDevice.Pause();                  //working playback stop button - doesnt crash when playback null 
+        }
+
+        private void butStop_click(object sender, EventArgs e)
+        {
+            outputDevice.Stop();      //throws exception if pressed when already stopped
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+            long playbackDuration = outputDevice.GetPosition();
+            string durationString;
+            durationString = playbackDuration.ToString();
+            textBox2.Text = durationString;
+
+        }
+
+        private void butPauseStop_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            if (numClicktime == 0)
+            {
+
+                doubleClickTimer.Interval = 50;
+                doubleClickTimer.Tick +=
+                new EventHandler(doubleClickTimer_Tick);
+
+                numClicktime = DateTime.Now.TimeOfDay.TotalMilliseconds;
+                doubleClickTimer.Start();
+
+            }
+            else 
+            {
+
+                numClicktime = 0;
+                doubleClickTimer.Stop();
+                outputDevice.Stop();     //double click event
+                //MessageBox.Show("DOUBLE");
+
+            }
+
+
+        }
+
+        void doubleClickTimer_Tick(object sender, EventArgs e)
+        {
+           
+            if (DateTime.Now.TimeOfDay.TotalMilliseconds-numClicktime > 300)
+            {
+                numClicktime = 0;
+                doubleClickTimer.Stop();
+                outputDevice.Pause();       //single click event
+                //MessageBox.Show("SINGLE");
+            
+            }
+                    
+        }
+
+        private void butPauseStop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void volumeSlider1_Load(object sender, EventArgs e)
+        {
+            //volumeSlider1.Volume = outputDevice.Volume();
+
+        }
+    }
+}
